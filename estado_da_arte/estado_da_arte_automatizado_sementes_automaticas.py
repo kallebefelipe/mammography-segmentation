1b1c5afc57be2e73c2c369cb4709ea0170ffe048
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-02 15:01:53 -0300

    Experimento com sementes geradas com evolucao

---
+++ estado_da_arte/estado_da_arte_automatizado_sementes_automaticas.py
@@ -0,0 +1,270 @@
+"""
+Fuzzy witch normalizaton
+"""
+
+import numpy as np
+from skimage.segmentation import random_walker
+from skimage import io
+import scipy.misc
+from skimage import segmentation
+import scipy.io
+from math import sqrt
+import math
+import csv
+
+beta = [10000, 20000, 30000, 40000]
+tol = [0.01, 0.02, 0.0001]
+mode = ['cg_mg', 'cg', 'bf']
+
+constante = [5]
+
+
+medias = []
+numero_marcacao = []
+sub_pasta = 5
+with open('../output/evolucao/resultado.csv', "w") as \
+        resultado:
+    output = csv.writer(resultado, quoting=csv.QUOTE_ALL)
+
+    cabecalho_1 = []
+    cabecalho_1.append("")
+    for a in beta:
+        for b in tol:
+            for c in mode:
+                titulo = str(' beta='+str(a)+' tol='+str(b) +
+                             ' mode='+str(c)+' ')
+                cabecalho_1.append(titulo)
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+                cabecalho_1.append('')
+
+    output.writerow(cabecalho_1)
+
+    cabecalho_2 = []
+    cabecalho_2.append('')
+    cont = 0
+    for a in beta:
+        for b in tol:
+            for c in mode:
+                cabecalho_2.append('TP')
+                cabecalho_2.append('TN')
+                cabecalho_2.append('FP')
+                cabecalho_2.append('FN')
+                cabecalho_2.append('Sensibility')
+                cabecalho_2.append('Especificy')
+                cabecalho_2.append('Jaccard')
+                cabecalho_2.append('TFP')
+                cont += 8
+    output.writerow(cabecalho_2)
+
+    images = ['mdb001', 'mdb002',  'mdb005', 'mdb010', 'mdb012',
+              'mdb013', 'mdb015', 'mdb017', 'mdb019', 'mdb021',
+              'mdb023', 'mdb025', 'mdb028', 'mdb030', 'mdb032',
+              'mdb058', 'mdb063', 'mdb069', 'mdb080', 'mdb091',
+              'mdb132_1', 'mdb132_2', 'mdb134', 'mdb141', 'mdb142',
+              'mdb144_1', 'mdb144_2', 'mdb145', 'mdb148', 'mdb175',
+              'mdb178', 'mdb179', 'mdb181', 'mdb184', 'mdb186',
+              'mdb188', 'mdb190', 'mdb191', 'mdb193', 'mdb195',
+              'mdb198', 'mdb199', 'mdb202', 'mdb204', 'mdb206',
+              'mdb207', 'mdb244', 'mdb264', 'mdb265', 'mdb267',
+              'mdb270', 'mdb271', 'mdb274', 'mdb290', 'mdb312',
+              'mdb314', 'mdb315']
+    images = ['mdb001', 'mdb002']
+
+    soma = [0] * cont
+    for i in images:
+        numbMarkx = 0
+        numbMarky = 0
+        posX = [0] * int(sub_pasta)
+        posY = [0] * int(sub_pasta)
+        posX_origem = [0] * int(sub_pasta)
+        posY_origem = [0] * int(sub_pasta)
+        image = io.imread('../data/imagens/'+i+'.bmp')
+        markers = np.zeros((image.shape[0], image.shape[1]))
+
+        with open('../evolucao/sementes/'+i+'.txt') as f:
+            data = f.readlines()
+            for item in data[:int(sub_pasta)]:
+                item = item.replace('\n', '')
+                item = item.split(',')
+                posX[numbMarkx] = int(item[2])
+                posY[numbMarky] = int(item[1])
+                posX_origem[numbMarkx] = int(item[2])
+                posY_origem[numbMarky] = int(item[1])
+                numbMarkx = numbMarkx + 1
+                numbMarky = numbMarky + 1
+        colb = 1
+        posX = list(map(lambda x: x/float(image.shape[0]), posX))
+        posY = list(map(lambda x: x/float(image.shape[1]), posY))
+
+        xm = 0
+        ym = 0
+        ax = 1
+        ay = 1
+        desviox = 0
+        desvioy = 0
+        constantex = 1
+        constantey = 1
+
+        for x in range(0, numbMarkx):
+            xm = xm + posX[x]
+        xm = xm / numbMarkx
+
+        for y in range(0, numbMarky):
+            ym = ym + posY[y]
+        ym = ym / numbMarky
+
+        somadorx = 0
+
+        for x in range(0, numbMarkx):
+            somadorx = somadorx + (math.pow((posX[x] - xm), 2))
+        desviox = sqrt(somadorx / float(numbMarkx-1))
+        somadory = 0
+
+        for y in range(0, numbMarky):
+            somadory = somadory + (math.pow((posY[y] - ym), 2))
+
+        desvioy = sqrt(somadory / float(numbMarky-1))
+
+        imageShape = np.ones((image.shape[0], image.shape[1]))
+
+        for x in range(0, image.shape[0]):
+            for y in range(0, image.shape[1]):
+                x_n = x/float(image.shape[0])
+                y_n = y/float(image.shape[1])
+                imageShape[x, y] = \
+                    math.exp(((-1)*math.pow((x_n-xm), 2)) / float(2 * constantex * desviox) + ((-1) * math.pow((y_n-ym), 2))/float(2*constantey*desvioy))
+
+        copia = io.imread('../data/imagens/' + i + '.bmp')
+
+        for x in range(0, image.shape[0]):
+            for y in range(0, image.shape[1]):
+                if(imageShape[x, y] > 0.5):
+                    copia[x, y] = 1
+                else:
+                    copia[x, y] = 0
+
+        contorno = segmentation.mark_boundaries(image, copia,
+                                                color=(1, 0, 0))
+
+        for x in range(0, image.shape[0]):
+            for y in range(0, image.shape[1]):
+                val2 = copia[x, y]
+                if val2 == 0:
+                    markers[x][y] = 2
+
+        for pos in range(0, int(sub_pasta)):
+
+            markers[posX_origem[pos]][posY_origem[pos]] = 1
+
+        ouro = io.imread('../data/imagens/'+i+'_bin.bmp')
+
+        linha = []
+        linha.append(i)
+
+        pos = 0
+        for a in beta:
+            for b in tol:
+                for c in mode:
+                    labels_rw = random_walker(image,
+                                              markers,
+                                              beta=a, tol=b, mode=c)
+                    name = '%d-%.4f-%s.jpg' % (a, b, c)
+                    contorno = \
+                        segmentation.mark_boundaries(image, ouro,
+                                                     color=(0, 0, 0))
+                    contorno = \
+                        segmentation.mark_boundaries(contorno,
+                                                     labels_rw,
+                                                     color=(0, 1, 0))
+                    name = \
+                        '../output/evolucao/imagens/'+i+'-'+str(a)+'-'+str(b) +\
+                        '-'+str(c)+'.jpg'
+                    scipy.misc.imsave(name, contorno)
+                    labels_rw = (2-labels_rw)
+
+                    TP = sum((labels_rw == 1) & (ouro == 255))
+                    somaTP = sum(TP)
+                    TN = sum((labels_rw == 0) & (ouro == 0))
+                    somaTN = sum(TN)
+                    FN = sum((labels_rw == 0) & (ouro == 255))
+
+                    FP = sum((labels_rw == 1) & (ouro == 0))
+                    somaFN = sum(FN)
+                    somaFP = sum(FP)
+
+                    linha.append(str(somaTP))
+                    soma[pos] += somaTP
+                    pos += 1
+                    linha.append(str(somaTN))
+                    soma[pos] += somaTN
+                    pos += 1
+                    linha.append(str(somaFP))
+                    soma[pos] += somaFP
+                    pos += 1
+                    linha.append(str(somaFN))
+                    soma[pos] += somaFN
+                    pos += 1
+
+                    XOR = (somaFP + somaFN)/(somaTP + somaFN)
+                    Precision = somaTP/float(somaTP + somaFP)
+                    Sensitivity = somaTP/float(somaTP + somaFN)
+                    Specificity = somaTN/float(somaFP + somaTN)
+                    Jaccard = somaTP/float(somaTP + somaFN + somaFP)
+                    Recall = somaTP/float(somaTP + somaFN)
+
+                    if (Precision != 0) and (Recall != 0):
+                        Fmeasure = \
+                            (Precision*Recall)/(Precision+Recall)
+                    else:
+                        Fmeasure = 0
+
+                    linha.append(str(Sensitivity))
+                    soma[pos] += Sensitivity
+                    pos += 1
+                    linha.append(str(Specificity))
+                    soma[pos] += Specificity
+                    pos += 1
+                    linha.append(str(Jaccard))
+                    soma[pos] += Jaccard
+                    pos += 1
+                    linha.append(str(1-Specificity))
+                    soma[pos] += 1-Specificity
+                    pos += 1
+        output.writerow(linha)
+    media = ['media']
+    for valor in soma:
+        media.append(str(valor/len(images)))
+    medias.append(media)
+    output.writerow(media)
+
+    with open('../output/evolucao/result_medias.csv',
+              "w") as result_medias:
+        output_2 = csv.writer(result_medias, quoting=csv.QUOTE_ALL)
+
+        cabecalho = ['']
+        cabecalho.append('TP')
+        cabecalho.append('TN')
+        cabecalho.append('FP')
+        cabecalho.append('FN')
+        cabecalho.append('Sensibility')
+        cabecalho.append('Especificy')
+        cabecalho.append('Jaccard')
+        cabecalho.append('TFP')
+        output_2.writerow(cabecalho)
+        pos_2 = 1
+        for a in beta:
+            for b in tol:
+                for c in mode:
+                    linha = []
+                    linha.append(' beta='+str(a)+' tol='+str(b) +
+                                 ' mode='+str(c)+' '
+                                 )
+                    for t in range(pos_2, pos_2+8):
+                        linha.append(media[t])
+                    pos_2 += 8
+                    output_2.writerow(linha)
--- evolucao/evolucao_diferencial.py
+++ evolucao/evolucao_diferencial.py
@@ -3,6 +3,7 @@ import scipy.misc
 import random
 import skimage
 import csv
+from datetime import datetime
 
 
 def fitnessFunction(individual, N, image):
@@ -43,21 +44,16 @@ images = ['mdb001', 'mdb002',  'mdb005', 'mdb010', 'mdb012',
           'mdb270', 'mdb271', 'mdb274', 'mdb290', 'mdb312',
           'mdb314', 'mdb315']
 
-images = ['mdb001']
-
 F = 1
 CR = [0.5, 0.6, 0.9]
 N = [5, 10, 15, 20]
 PopulationSize = [10, 20, 30]
-CR = [0.5]
-N = [5]
-PopulationSize = [10]
-
-
 interation = 10
 
 
-for pasta in range(1, 2):
+medias_interacao = []
+desvios_interacao = []
+for pasta in range(1, interation+1):
     with open('resultados/'+str(pasta)+'/resultado/resultado.csv', "w") as \
                 resultado:
         output = csv.writer(resultado, quoting=csv.QUOTE_ALL)
@@ -72,14 +68,15 @@ for pasta in range(1, 2):
                              ', Pop= '+str(population_size) + \
                              ', Sement='+str(n)
                     cabecalho_1.append(titulo)
-                    cabecalho_1.append('')
                     count += 1
         output.writerow(cabecalho_1)
 
-        planilha = []
         soma = [0] * count
         matriz_resultado = []
-        for path in images:
+        for count, path in enumerate(images):
+            print('Interacao: '+str(pasta)+' | Image: '+str(count))
+            comeco = datetime.now()
+
             row = []
             row_result = []
             row.append(path)
@@ -102,7 +99,6 @@ for pasta in range(1, 2):
 
                         i = 0
                         while (i < 3000):
-                            print(i)
                             i += 1
                             j = 0
                             while (j < population_size):
@@ -157,10 +153,22 @@ for pasta in range(1, 2):
                         image_rgb = skimage.color.grey2rgb(image)
                         for i in range(0, n):
                             image_rgb[bestFitness[i][0]][bestFitness[i][1]] = [255, 0, 0]
-                            image_rgb[bestFitness[i][0]][bestFitness[i][1]-1] = [255, 0, 0]
-                            image_rgb[bestFitness[i][0]][bestFitness[i][1]+1] = [255, 0, 0]
-                            image_rgb[bestFitness[i][0]+1][bestFitness[i][1]] = [255, 0, 0]
-                            image_rgb[bestFitness[i][0]-1][bestFitness[i][1]] = [255, 0, 0]
+                            try:
+                                image_rgb[bestFitness[i][0]][bestFitness[i][1]-1] = [255, 0, 0]
+                            except:
+                                pass
+                            try:
+                                image_rgb[bestFitness[i][0]][bestFitness[i][1]+1] = [255, 0, 0]
+                            except:
+                                pass
+                            try:
+                                image_rgb[bestFitness[i][0]+1][bestFitness[i][1]] = [255, 0, 0]
+                            except:
+                                pass
+                            try:
+                                image_rgb[bestFitness[i][0]-1][bestFitness[i][1]] = [255, 0, 0]
+                            except:
+                                pass
                         scipy.misc.imsave('resultados/'+str(pasta) +
                                           '/images/'+path+'-'+str(cr) +
                                           '-'+str(n)+'-'+str(population_size) +
@@ -172,25 +180,85 @@ for pasta in range(1, 2):
                         row_result.append(float(percent))
                         pos_soma += 1
             matriz_resultado.append(row_result)
-            planilha.append(row)
+            output.writerow(row)
+            fim = datetime.now()
+            print(fim-comeco)
 
         row = ['media']
         media = []
         for som in soma:
             row.append(str(som/len(images)))
             media.append(som/len(images))
-        planilha.append(row)
+        output.writerow(row)
+        medias_interacao.append(media)
 
         desvio = []
-        somatorio = 0
         for col in range(0, len(matriz_resultado[0])):
+            somatorio = 0
             for lin in range(0,     len(matriz_resultado)):
                 somatorio += (matriz_resultado[lin][col]-media[col])**2
-            desvio.append((somatorio/len(images)-1)**(1/2))
+
+            if len(images) == 1 or somatorio == 0:
+                desvio.append(0)
+            else:
+                desvio.append((somatorio/len(images)-1)**(1/2))
+        desvios_interacao.append(desvio)
 
         row = ['desvio']
         for each in desvio:
             row.append(each)
 
-        planilha.append(row)
-        output.writerow(planilha)
+        output.writerow(row)
+
+
+with open('resultados/resultado_final.csv', "w") as \
+                resultado:
+    output = csv.writer(resultado, quoting=csv.QUOTE_ALL)
+
+    cabecalho_1 = ['']
+    for cr in CR:
+        for n in N:
+            for population_size in PopulationSize:
+                titulo = 'F= '+str(F)+', CR= '+str(cr) + \
+                         ', Pop= '+str(population_size) + \
+                         ', Sement='+str(n)
+                cabecalho_1.append(titulo)
+    output.writerow(cabecalho_1)
+
+    soma = [0] * len(medias_interacao[0])
+
+    for inter in range(0, interation):
+        for count, med in enumerate(medias_interacao[inter]):
+            soma[count] += med
+
+    row = ['media']
+    for som in soma:
+        row.append(str(som/interation))
+    output.writerow(row)
+
+    soma = [0] * len(desvios_interacao[0])
+
+    for inter in range(0, interation):
+        for count, med in enumerate(desvios_interacao[inter]):
+            soma[count] += med
+
+    media = []
+    for som in soma:
+        media.append(som/interation)
+
+    desvio = []
+    for col in range(0, len(desvios_interacao[0])):
+        somatorio = 0
+        for lin in range(0, interation):
+            somatorio += (desvios_interacao[lin][col]-media[col])**2
+
+        if somatorio == 0:
+            desvio.append(0)
+        else:
+            desvio.append((somatorio/interation-1)**(1/2))
+
+    row = ['desvio']
+    for each in desvio:
+        row.append(each)
+
+    output.writerow(row)
